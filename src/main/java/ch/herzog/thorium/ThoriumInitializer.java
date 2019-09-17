226820394d97e9f3765b336cf9d5546b103549a3
Thomas <batthomas@gmx.ch>
2019-09-17 17:29:46 +0200

    [+] First Commit

---
+++ src/main/java/ch/herzog/thorium/ThoriumInitializer.java
@@ -0,0 +1,12 @@
+package ch.herzog.thorium;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ThoriumInitializer implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello Thorium!");
+    }
+
+}
